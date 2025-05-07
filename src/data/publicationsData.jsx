d5e2b7b65ef1266088bdd5f6268dc4ae1b518f7a
elisaahlback <ahlbackelisa@gmail.com>
2025-05-07 19:23:07 +0200

    Create data file and add information of each publication

---
+++ src/data/publicationsData.jsx
@@ -0,0 +1,87 @@
+import articleIcon from '.././assets/article-icon.svg';
+import book1 from '.././assets/Category1.jpg';
+import book2 from '.././assets/Category2.jpg';
+import book3 from '.././assets/Category2a.jpg';
+import book4 from '.././assets/Category2b.jpg';
+import book5 from '.././assets/Category2c.jpg';
+
+const publicationsData = [
+    {
+        title: "Hochschullehre postdigital",
+        subtitle: "Lehren und Lernen neu gestalten",
+        description: "Feil, S., Frie, C. Steppert, M. & Vahlbruch, J.-W. (2024). Flipped Classroom als Fachkundekurs im Strahlenschutz: Claas-Welt. In: Vöing, T. Jenert, I. Neiske, J. Osthushenrich, U. Trier, T. Weber & K. Altroggen (Hg.) (2024). Hochschullehre postdigital. Lehren und Lernen neu gestalten. wbv, Bielefeld.",
+        isbn: "978-3-7639-7718-5",
+        doi: "DOI: 10.3278/9783763977192",
+        image: book1,
+        pubid: "pub1"
+    },
+    {
+        title: "Didaktische Handreichung zur praktischen Nutzung von KI in der Lehre",
+        subtitle: "Arbeitsgruppe Digitale Medien und Hochschuldidaktik der Deutschen Gesellschaft für Hochschuldidaktik in Kooperation mit der Gesellschaft für Medien in der Wissenschaft",
+        description: "Bremer, C., Eichhorn, M., Feil, S., Haberer, M., Hawlitschek, A., Lohner, D., Mandausch, M. Neiske, I., Pohl, H.-M., Schmitz, B. Sperl, A. & Watolla, A.-K. (2024). Didaktische Handreichung zur praktischen Nutzung von KI in der Lehre. Arbeitsgruppe „Digitale Medien und Hochschuldidaktik” der Deutschen Gesellschaft für Hochschuldidaktik (dghd) in Kooperation mit der Gesellschaft für Medien in der Wissenschaft (GMW)",
+        isbn: "",
+        doi: "",
+        image: articleIcon,
+        imageSize: 'small',
+        pubid: "pub2"
+      
+    },
+    {
+        title: "Den Stein ins Rollen bringen – wenn Geschichten das Lernen aktivieren",
+        subtitle: "Digitales Storytelling in der Erwachsenenbildung",
+        description: "Feil, S. (2020). Den Stein ins Rollen bringen - wenn Geschichten das Lernen aktivieren. Digitales Storytelling in der Erwachsenenbildung.In: Erwachsenenbildung: Vierteljahresschrift für Theorie und Praxis, 66 (2020) 4, S. 158-161",
+        isbn: "",
+        doi: "https://doi.org/10.13109/erbi.2020.66.4.158",
+        image: articleIcon,
+        imageSize: 'small',
+        pubid: "pub3"
+    },
+    {
+        title: "Brückenkurs Chemie",
+        subtitle: "Kompaktes Wissen für den Studienstart",
+        description: "Feil, S., Engelmann, P., Wilke, T. & Wolf, K. (2024). Brückenkurs Chemie. Kompaktes Wissen für den Studienstart. Springer Spektrum Berlin, Heidelberg.",
+        isbn: "978-3-662-69350-6",
+        doi: "DOI: 10.1007/978-3-662-69351-3 ",
+        image: book5,
+        pubid: "pub4"
+    },
+    {
+        title: "Fliegenfüße, Sonnencreme, Pfefferminz und Kerzenschein",
+        subtitle: "Mit Bio und Chemie durchs Jahr",
+        description: "Feil, S. (2019). Fliegenfüße, Sonnencreme, Pfefferminz und Kerzenschein. Mit Bio und Chemie durchs Jahr. Springer, Berlin, Heidelberg.",
+        isbn: "978-3-622-59263-2",
+        doi: "DOI: 10.1007/978-3-662-59264-9",
+        image: book4,
+        pubid: "pub5"
+    },
+    {
+        title: "Faszinierende Chemie",
+        subtitle: "Eine Entdeckungsreise vom Ursprung der Elemente bis zur modernen Chemie",
+        description: "Feil, S., Resag, J. & Riebe, K. (2018). Faszinierende Chemie. Eine Entdeckungsreise vom Ursprung der Elemente bis zur modernen Chemie. Springer Verlag, Heidelberg, 2. Auflage",
+        isbn: "978-3-662-49920-7",
+        doi: "DOI: 10.1007/978-3-662-57324-2",
+        image: book3,
+        pubid: "pub6"
+    },
+    {
+        title: "Labskaus – Dosenfutter und Meer",
+        subtitle: "",
+        description: "Feil, S. (2018). Labskaus – Dosenfutter und Meer. In A. Ghadiri, Th. Vilgis & Th. Bosbach (2018). Wissen schmeckt. Die Magie der Wissenschaften beim Kochen erklärt – mit 16 Rezepten. Springer, Wiesbaden, S. 233-253.",
+        isbn: "978-3-658-21389-3",
+        doi: "DOI: 10.1007/978-3-658-21390-9_12",
+        image: book2,
+        pubid: "pub7"
+    },
+    {
+        title: "",
+        subtitle: "",
+        description: "Teilübersetzung Englisch-Deutsch des Standardlehrwerks „Organische Chemie“, K. P. C. Vollhardt, N. E. Schore, Verlag WILEY-VCH, 4. Auflage, 2005",
+        isbn: "978-3-527-31380-8",
+        doi: "",
+        image: articleIcon,
+        imageSize: 'small',
+        pubid: "pub8"
+    }
+  ];
+  
+  export default publicationsData;
