eaa170d647142080ca77ef7ef286cac1d749772f
elisaahlback <ahlbackelisa@gmail.com>
2025-05-07 19:32:10 +0200

    Change navbar to German and change the website palette orange to red as specified in the project overview document

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,4 +1,4 @@
-import logoSylvia from '.././assets/Logo-Sylvia-Feil.svg'
+import LogoSylvia from '.././assets/Logo-Sylvia-Feil.svg'
 import { Link } from 'react-router-dom'
 
 function Navbar() {
@@ -6,15 +6,15 @@ function Navbar() {
       <nav className="fixed top-0 left-0 w-full z-50 bg-light text-teal px-6 py-0.5 flex items-center justify-between drop-shadow-xs">
         <div className="text-xl font-bold">
             <Link to="/">
-                  <img src={logoSylvia} className="logo h-7" alt="Sylvia Feil logo" />
+                  <img src={LogoSylvia} className="logo h-7" alt="Sylvia Feil logo" />
             </Link>
         </div>
         <ul className="flex gap-6">
-          <li><Link to="/science_communication" className="text-red-600 hover:text-teal-400">Science communication</Link></li>
-          <li><Link to="/media_didactics" className="text-teal hover:text-teal-400">Media didactics</Link></li>
-          <li><Link to="/publications" className="text-teal hover:text-teal-400">Publications</Link></li>
-          <li><Link to="/vita" className="text-teal hover:text-teal-400">Vita</Link></li>
-          <li><Link to="/impressum" className="text-teal hover:text-teal-400">Impressum</Link></li>
+          <li><Link to="/science_communication" className="text-teal hover:text-red transition-all duration-300 ease-in-out">Wissenschaftskommunikation</Link></li>
+          <li><Link to="/media_didactics" className="text-teal hover:text-red transition-all duration-300 ease-in-out">Mediendidaktik</Link></li>
+          <li><Link to="/publications" className="text-teal hover:text-red transition-all duration-300 ease-in-out">Publikationen</Link></li>
+          <li><Link to="/vita" className="text-teal hover:text-red transition-all duration-300 ease-in-out">Vita</Link></li>
+          <li><Link to="/impressum" className="text-teal hover:text-red transition-all duration-300 ease-in-out">Impressum</Link></li>
         </ul>
       </nav>
     );
